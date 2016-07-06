8bfef9c2747c9397696977cb55e6319818a940fe
Jkail <boutaultbenoit@yahoo.fr>
2016-07-06 23:24:27 +0200

    V1 + version qui marche pas de moi

---
+++ js/konamicode.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Jkail on 06/07/2016.
+ */
+
+document.addEventListener("keydown", function(e) {
+        var konami = "38,38,40,40,37,39,37,39,66,65";
+        var frappe = String.fromCharCode(e.charCode);
+        var saisie = saisie + [frappe];
+        var verification = saisie.indexOf(konami);
+        if (verification != "-1") {
+            //Code JS
+            window.location = "http://benoit-boutault.fr";
+        }
+    });
+ /*
+if ( window.addEventListener ) {
+    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+    window.addEventListener("keydown", function (e) {
+            kkeys.push(e.keyCode);
+            if (kkeys.toString().indexOf(konami) >= 0)
+                window.location = "http://ejohn.org/apps/hero/";
+        },
+        true);
+}
+     */
