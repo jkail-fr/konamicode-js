93410c94748d14cb47b6fb72ac831b05483b4741
Jkail <boutaultbenoit@yahoo.fr>
2016-07-06 23:46:50 +0200

    amelioration du js ... marche toujours pas

--- js/konamicode.js
+++ js/konamicode.js
@@ -5,9 +5,10 @@
 document.addEventListener("keydown", function(e) {
         var konami = "38,38,40,40,37,39,37,39,66,65";
         var frappe = String.fromCharCode(e.charCode);
-        var saisie = saisie + [frappe];
+        //var saisie = saisie + [frappe];
+        saisie.push(e.charCode);
         var verification = saisie.indexOf(konami);
-        if (verification != "-1") {
+        if (verification >= 0) {
             //Code JS
             window.location = "http://benoit-boutault.fr";
         }
