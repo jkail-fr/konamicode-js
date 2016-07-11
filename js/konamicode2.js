774331806de98ef2ef7f27b4858c228fce4e6212
Jkail <boutaultbenoit@yahoo.fr>
2016-07-11 23:40:40 +0200

    La v2 marche. il n'y avait pas besoin de .lenght

--- js/konamicode2.js
+++ js/konamicode2.js
@@ -7,7 +7,7 @@ var konami = "38,38,40,40,37,39,37,39,66,65";
 document.addEventListener("keydown", function(e) {
     var frappe = String.fromCharCode(e.charCode);
     var i =0
-    while ((frappe = konami.lenght[i]) && (i > 9 )) {
+    while ((frappe = konami[i]) && (i > 9 )) {
         i++
     }
     window.location = "http://benoit-boutault.fr";
