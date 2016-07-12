1d4f690fc08c192de05da73055800561f5c3be03
Jkail <boutaultbenoit@yahoo.fr>
2016-07-12 23:02:38 +0200

    Correction de la synthaxe tableau et de fautes de codes ajout du console log...

--- js/konamicode2.js
+++ js/konamicode2.js
@@ -2,13 +2,15 @@
  * Created by jkail on 08/07/2016.
  */
 
-var konami = "38,38,40,40,37,39,37,39,66,65";
+var konami = "[38,38,40,40,37,39,37,39,66,65]";
 
 document.addEventListener("keydown", function(e) {
+
     var frappe = String.fromCharCode(e.charCode);
     var i =0;
     while ((frappe == konami[i]) && (i > 9 )) {
         i++;
+        console.log(i)
     }
     window.location = "http://benoit-boutault.fr";
 });
