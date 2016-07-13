265cf0938e71be1933e732033bf68158a90fffbd
jkail-fr <boutaultbenoit@yahoo.fr>
2016-07-13 13:46:15 +0200

    Correction : Retrait du while ajout du double if

--- js/konamicode2.js
+++ js/konamicode2.js
@@ -2,15 +2,24 @@
  * Created by jkail on 08/07/2016.
  */
 
-var konami = "[38,38,40,40,37,39,37,39,66,65]";
+var konami = [38,38,40,40,37,39,37,39,66,65];
+var i =0;
 
 document.addEventListener("keydown", function(e) {
 
-    var frappe = String.fromCharCode(e.charCode);
-    var i =0;
-    while ((frappe == konami[i]) && (i > 9 )) {
+    var frappe = e.keyCode;
+
+    if (frappe == konami[i]) {
         i++;
-        console.log(i)
+
+        }
+    else {
+        i = 0;
+
     }
-    window.location = "http://benoit-boutault.fr";
+
+    if (i > 9) {
+        window.location = "http://benoit-boutault.fr";
+    }
+
 });
